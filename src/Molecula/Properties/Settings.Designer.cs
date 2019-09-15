568ffba4d69cc2c10b6a95e6d9be7d358ae1d803
Steffen Rossberg <srossberg@gmx.net>
2019-09-15 22:13:36 +0200

    added settings of systems in login dialog

--- src/Molecula/Properties/Settings.Designer.cs
+++ src/Molecula/Properties/Settings.Designer.cs
@@ -8,22 +8,57 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Molecula.Properties
-{
-
-
+namespace Molecula.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("MOLTEST")]
+        public string AvailableSystem0 {
+            get {
+                return ((string)(this["AvailableSystem0"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("MOLVAL")]
+        public string AvailableSystem1 {
+            get {
+                return ((string)(this["AvailableSystem1"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("MOLPROD")]
+        public string AvailableSystem2 {
+            get {
+                return ((string)(this["AvailableSystem2"]));
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("TEST")]
+        public string LastSelectedSystem {
+            get {
+                return ((string)(this["LastSelectedSystem"]));
+            }
+            set {
+                this["LastSelectedSystem"] = value;
             }
         }
     }
