ae8bab3beaf653322fd7bfdcfc7690bff6746030
Steffen Rossberg <srossberg@gmx.net>
2019-10-11 08:05:08 +0200

    introduced AbstractViewModel

---
+++ src/Molecula.Abstractions/ViewModels/IAbstractViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Pamucuk.Mvvm.ViewModels;
+
+namespace Molecula.Abstractions.ViewModels
+{
+    public interface IAbstractViewModel : IViewModelBase
+    {
+        string CurrentFocusId { get; }
+
+        void SetCurrentFocusId(string focusId);
+    }
+}
---
+++ src/Molecula/ViewModels/AbstractViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Molecula.Abstractions.ViewModels;
+using Pamucuk.Mvvm.Observables;
+
+namespace Molecula.ViewModels
+{
+    public abstract class AbstractViewModel : ObservableObject, IAbstractViewModel
+    {
+        public abstract string ViewModelId { get; }
+
+        private string _currentFocusId;
+        public string CurrentFocusId => _currentFocusId;
+
+        public void SetCurrentFocusId(string focusId)
+        {
+            Set(ref _currentFocusId, null, nameof(CurrentFocusId));
+            Set(ref _currentFocusId, focusId, nameof(CurrentFocusId));
+        }
+    }
+}
